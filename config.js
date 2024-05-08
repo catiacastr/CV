03eea928e081d6e4047008d3b975313aa92ceddc
catiacastr <catia.castro5@gmail.com>
2024-05-08 09:44:39 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  url: 'https://gatsby-starter-blog-and-cv.netlify.app',
+  pathPrefix: '/',
+  title: 'Blog by Cátia Castro',
+  keywords:
+    'account surfer, trello bookmark, trello, productivity, javascript, Cátia Castro, choosy windows, browser selector',
+  subtitle: 'Developer. Optimizer. Learner.',
+  copyright: '© 2020 | Kyiv, Ukraine',
+  disqusShortname: 'dospolov',
+  postsPerPage: 20,
+  googleAnalyticsId: 'UA-6589522-7',
+  menu: [
+    {
+      label: 'Blog',
+      path: '/'
+    },
+    {
+      label: 'CV',
+      path: '/cv'
+    }
+  ],
+  author: {
+    name: 'Cátia Castro',
+    photo: '/photo.png',
+    bio: 'Backend Developer Intermediate | Business Inteligence Developer | Data & Analytics | PowerBI | DAX | Figma',
+    contacts: {
+      github: 'dospolov',
+      twitter: 'marat_dospolov',
+      linkedin: 'dospolov',
+      telegram: 'dospolov',
+      instagram: 'dospolov',
+      facebook: 'dospolov',
+      email: '',
+      rss: '/rss.xml'
+    }
+  }
+}
